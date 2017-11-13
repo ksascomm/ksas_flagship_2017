8b40c9afd9d40e7643354139528e528503821bce
ksascomm <ksasweb@jhu.edu>
2017-11-13 14:56:49 -0500

    increase height of hub-news stories with x-long excerpts

--- template-parts/hub-news.php
+++ template-parts/hub-news.php
@@ -16,7 +16,7 @@
 	$hub_results = json_decode($hub_call['body'], true);
 	$hub_articles = $hub_results['_embedded'];
 	foreach ($hub_articles['articles'] as $hub_article ) { ?>
-	<article class="hub-news" aria-labelledby="post-<?php echo $hub_article['id'];?>">
+	<article class="hub-news story end" aria-labelledby="post-<?php echo $hub_article['id'];?>">
 		<img class="hub-img" src="<?php echo $hub_article['_embedded']['image_thumbnail'][0]['sizes']['thumbnail']; ?>" alt="From The Hub: <?php echo $hub_article['headline']; ?>" />
 		<h1><a href="<?php echo $hub_article['url']; ?>" id="post-<?php echo $hub_article['id'];?>"><?php echo $hub_article['headline']; ?></a></h1>
 		<summary>
