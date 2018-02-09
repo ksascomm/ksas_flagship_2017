5b5fa701a922c8f079d00c760ff378dd12e5ecd9
ksascomm <ksasweb@jhu.edu>
2018-02-09 13:50:48 -0500

    remove console.log on success; add alt tag to image

--- template-parts/feed-instagram.php
+++ template-parts/feed-instagram.php
@@ -9,16 +9,16 @@
     type: 'GET',
     data: {access_token: token, q: username}, // actually it is just the search by username
     success: function(data){
-        console.log(data);
+        //console.log(data);
         $.ajax({
             url: 'https://api.instagram.com/v1/users/' + data.data[0].id + '/media/recent', // specify the ID of the first found user
             dataType: 'jsonp',
             type: 'GET',
             data: {access_token: token, count: num_photos},
             success: function(data2){
-                console.log(data2);
+               ///console.log(data2);
                 for(x in data2.data){
-                    $('.instagram-images').append('<div class="small-12 medium-6 large-3 columns instagram-card"><img class="gram" src="'+data2.data[x].images.thumbnail.url+'"></div>');  
+                    $('.instagram-images').append('<div class="small-12 medium-6 large-3 columns instagram-card"><img class="gram" alt="'+data2.data[x].caption.text+'" src="'+data2.data[x].images.thumbnail.url+'"></div>');  
                 }
                 },
             error: function(data2){
