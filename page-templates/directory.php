9140acb7990683385fd32b355b31fdf25dd23951
ksascomm <ksasweb@jhu.edu>
2018-03-27 15:09:27 -0400

    remove transient

--- page-templates/directory.php
+++ page-templates/directory.php
@@ -5,8 +5,7 @@ Template Name: Dean's Directory
 get_header(); ?>
 
 <?php get_template_part( 'template-parts/featured-image' ); ?>
-<?php // if ( false === ( $flagship_leadership_query = get_transient( 'flagship_leadership_query' ) ) ) {
-		// It wasn't there, so regenerate the data and save the transient
+<?php
 		$flagship_leadership_query = new WP_Query(array(
 			'post_type' => 'people',
 			'role' => 'leadership',
@@ -15,7 +14,6 @@ get_header(); ?>
 			'order' => 'ASC',
 			'posts_per_page' => -1,
 			));
-			// set_transient( 'flagship_leadership_query', $flagship_leadership_query, 2592000 ); }
 		?> 
 
 
@@ -23,33 +21,33 @@ get_header(); ?>
 	<?php foundationpress_breadcrumb();?>
 
 	<div class="main-content">
-	<?php do_action( 'foundationpress_before_content' ); ?>
+		<?php do_action( 'foundationpress_before_content' ); ?>
 
-	<?php while ( have_posts() ) : the_post(); ?>
+		<?php while ( have_posts() ) : the_post(); ?>
 
-	  <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-	      <header aria-label="<?php the_title(); ?>">
-	          <h1 class="entry-title"><?php the_title(); ?></h1>
-	      </header>
-	      <?php do_action( 'foundationpress_page_before_entry_content' ); ?>
-	      <div class="entry-content">
-	          <?php the_content(); ?>
-	      </div>
-	  </article>
+		  <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+		      <header aria-label="<?php the_title(); ?>">
+		          <h1 class="entry-title"><?php the_title(); ?></h1>
+		      </header>
+		      <?php do_action( 'foundationpress_page_before_entry_content' ); ?>
+		      <div class="entry-content">
+		          <?php the_content(); ?>
+		      </div>
+		  </article>
 
-	<?php endwhile;?>
+		<?php endwhile;?>
 		<div class="row">
 			<ul class="directory">
 				<?php while ($flagship_leadership_query->have_posts() ) : $flagship_leadership_query->the_post(); ?>
-						<li class="item person">
-							<div class="media-object">
-								<?php if ( has_post_thumbnail() ) { ?> 
-									<div class="media-object-section">
-										<a href="<?php the_permalink();?>"><?php the_post_thumbnail('directory'); ?>
-										</a>							
-									</div>
-								<?php } ?>	
+					<li class="item person">
+						<div class="media-object">
+							<?php if ( has_post_thumbnail() ) { ?> 
 								<div class="media-object-section">
+									<a href="<?php the_permalink();?>"><?php the_post_thumbnail('directory'); ?>
+									</a>							
+								</div>
+							<?php } ?>	
+							<div class="media-object-section">
 								<h4 class="no-margin">
 									<a href="<?php the_permalink();?>"><?php the_title(); ?></a>
 								</h4>
@@ -70,9 +68,9 @@ get_header(); ?>
 										<li><span class="fa fa-map-marker"></span> <?php echo get_post_meta($post->ID, 'ecpt_office', true); ?></li>
 									<?php endif; ?>
 								</ul>
-								</div>
 							</div>
-						</li>		
+						</div>
+					</li>		
 				<?php endwhile; ?>
 			</ul>
 		</div>
