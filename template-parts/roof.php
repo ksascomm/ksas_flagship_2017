585147f67be2086615789d50fb915e06520f90a4
ksascomm <ksasweb@jhu.edu>
2019-10-14 14:45:27 -0400

    update title and links in roof.php

--- template-parts/roof.php
+++ template-parts/roof.php
@@ -23,19 +23,18 @@
 	</li>
 	<li class="roof-padding bar"><a href="https://www.jhu.edu/admissions/visit/" target="_blank" rel="noopener" aria-label="Visit Us">Visit</a></li>
 	<li class="roof-padding"><a href="https://krieger.jhu.edu/magazine/" target="_blank" rel="noopener" aria-label="Arts & Sciences Magazine">A&S Magazine</a></li>
-	<li><a class="button" href="#" aria-label="Explore KSAS" data-toggle="offCanvasTop1">Explore KSAS <span class="fa fa-bars" aria-hidden="true"></span></a></li>
+	<li><a class="button" href="#" aria-label="Explore KSAS" data-toggle="offCanvasTop1">Explore JHU <span class="fa fa-bars" aria-hidden="true"></span></a></li>
 </ul>
 
 <div class="off-canvas position-top" id="offCanvasTop1" data-off-canvas aria-hidden="true">
 	<div id="global-links" class="row small-up-2 medium-up-3 large-up-3">
 	<h1 class="show-for-sr">Explore KSAS</h1>
 		<div class="column column-block">
-			<h2>Academics</h2>
+			<h2>Inside the Krieger School</h2>
 			<ul class="vertical menu" role="menu">
 				<li role="menuitem"><a href="<?php echo site_url(); ?>/academics/departments-programs-and-centers/" onclick="ga('send', 'event', 'Offcanvas', 'Academics', 'Departments')">Departments, Programs, and Centers</a></li>
 				<li role="menuitem"><a href="<?php echo site_url(); ?>/people/faculty-directory/" onclick="ga('send', 'event', 'Offcanvas', 'Academics', 'Faculty')">Faculty Directory</a></li>
 				<li role="menuitem"><a href="<?php echo site_url(); ?>/academics/fields/" onclick="ga('send', 'event', 'Offcanvas', 'Academics', 'Fields of Study')">Fields of Study</a></li>
-				<li role="menuitem"><a href="https://www.library.jhu.edu/" onclick="ga('send', 'event', 'Offcanvas', 'Academics', 'Libraries')">Libraries</a></li>
 				<li role="menuitem"><a href="https://krieger.jhu.edu/academics/majors-minors/" onclick="ga('send', 'event', 'Offcanvas', 'Academics', 'Majors & Minors')">Majors & Minors</a></li>
 			</ul>
 		</div>
@@ -44,9 +43,8 @@
 			<ul class="vertical menu" role="menu">
 				<li role="menuitem"><a href="https://sis.jhu.edu/sswf/" onclick="ga('send', 'event', 'Offcanvas', 'Resources', 'ISIS')">Course Listings & Registration</a></li>
 				<li role="menuitem"><a href="https://www.jhu.edu/admissions/financial-aid/" onclick="ga('send', 'event', 'Offcanvas', 'Resources', 'Financial Aid')">Financial Aid</a></li>
-				<li role="menuitem"><a href="https://hrnt.jhu.edu/" onclick="ga('send', 'event', 'Offcanvas', 'Resources', 'Human Resources')">Human Resources</a></li>
 				<li role="menuitem"><a href="https://studentaffairs.jhu.edu/registrar" onclick="ga('send', 'event', 'Offcanvas', 'Resources', 'Registrars')">Registrar's Office</a></li>
-				<li role="menuitem"><a href="https://studyabroad.jhu.edu/" onclick="ga('send', 'event', 'Offcanvas', 'Resources', 'Study Abroad')">Study Abroad Office</a></li>
+				<li role="menuitem"><a href="https://policies.jhu.edu/" onclick="ga('send', 'event', 'Offcanvas', 'Resources', 'Registrars')">University Policies & Document Library <span class="fas fa-sign-in-alt"></span></a></li>	
 			</ul>
 		</div>
 		<div class="column column-block">
@@ -56,7 +54,6 @@
 				<li role="menuitem"><a href="https://www.jhu.edu/" onclick="ga('send', 'event', 'Offcanvas', 'Campus', 'JHU Home')">Johns Hopkins University Website</a></li>
 				<li role="menuitem"><a href="https://www.jhu.edu/maps-directions/" onclick="ga('send', 'event', 'Offcanvas', 'Campus', 'Maps/Directions')">Maps & Directions</a></li>
 				<li role="menuitem"><a href="https://my.jh.edu/portal/web/jhupub" onclick="ga('send', 'event', 'Offcanvas', 'Campus', 'myJHU')">myJHU</a></li>
-				<li role="menuitem"><a href="https://hub.jhu.edu/" onclick="ga('send', 'event', 'Offcanvas', 'Campus', 'TheHub')">The Hub</a></li>
 			</ul>
 		</div>
 		<button class="close-button" aria-label="Close menu" type="button" data-close>
