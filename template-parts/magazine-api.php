cdfff67b7ff061ad7948c62e3c92fbc29a614d1e
ksascomm <ksasweb@jhu.edu>
2019-11-20 14:09:53 -0500

    store magazine API call in a transient
    rename variables to be more semantic

--- template-parts/magazine-api.php
+++ template-parts/magazine-api.php
@@ -1,18 +1,26 @@
 <?php
+	$frontpagefeaturedimg_url = 'https://magazine.krieger.jhu.edu/wp-json/wp/v2/pages?_embed&slug=home';
 	
-	$frontpageresponse = wp_remote_get('https://magazine.krieger.jhu.edu/wp-json/wp/v2/pages?_embed&slug=home');
+	if ( false === ( $frontpageimage = get_transient( 'asmagazine_image_query' ) ) ) {
+		$frontpageimage = wp_remote_get($frontpagefeaturedimg_url);
+		set_transient( 'asmagazine_image_query', $frontpageimage, 2419200 ); 
+	}		
+	
 	// Display a error nothing is returned.
-	if ( is_wp_error( $frontpageresponse ) ) {
-		$error_string = $frontpageresponse->get_error_message();
+	if ( is_wp_error( $frontpageimage ) ) {
+		$error_string = $frontpageimage->get_error_message();
 		echo '<div class="callout alert"><p>' . $error_string . '</p></div>';
 
 	}
 	// Get the body.
-	$frontpost = json_decode( wp_remote_retrieve_body( $frontpageresponse ) );
+	
+	$frontpost = json_decode( wp_remote_retrieve_body( $frontpageimage ) );
+
 	// Display a warning nothing is returned.
 	if ( empty( $frontpost ) ) {
 		echo '<div class="callout warning"><p>There is no home page featured image</p></div>';
 	}
+	
 	// If there is a featured image for the home page, display it!
 	if ( ! empty( $frontpost ) ) :?>
 		<?php foreach ( $frontpost as $front ) : ?>
@@ -23,35 +31,41 @@
 				</div>
 			</div>
 		</div>
-	<?php endforeach;?>
+		<?php endforeach;?>
 	<?php endif;?>
 <?php
+
 	//get the Feature taxonomy ID
-	$response = wp_remote_get( 'https://magazine.krieger.jhu.edu/wp-json/wp/v2/pages?per_page=4&volume=114' );
+	$latest_features_url = 'https://magazine.krieger.jhu.edu/wp-json/wp/v2/pages?per_page=4&volume=114';
+
+	if ( false === ( $latest_features = get_transient( 'asmagazine_features_query' ) ) ) {
+		$latest_features = wp_remote_get($latest_features_url);
+		set_transient( 'asmagazine_features_query', $latest_features, 2419200 ); 
+	}	
 	
 	// Display a error nothing is returned.
-	if ( is_wp_error( $response ) ) {
-		$error_string = $response->get_error_message();
+	if ( is_wp_error( $latest_features ) ) {
+		$error_string = $latest_features->get_error_message();
 		echo '<div class="callout alert"><p>' . $error_string . '</p></div>';
 	}
 
 	// Get the body.
-	$posts = json_decode( wp_remote_retrieve_body( $response ) );
+	$features = json_decode( wp_remote_retrieve_body( $latest_features ) );
 	// Display a warning nothing is returned.
-	if ( empty( $posts ) ) {
+	if ( empty( $features ) ) {
 		echo '<div class="callout warning"><p>There is no content</p></div>';
 	}
 	// If there are posts then display them!
-	if ( ! empty( $posts ) ) :?>
+	if ( ! empty( $features ) ) :?>
 		
 		<div class="small-12 medium-8 xlarge-9 columns">
 			<div class="issue-stories">
 				<h2>Fall 2019 Features</h2>
-			<?php foreach ( $posts as $post ) : ?>
+			<?php foreach ( $features as $feature ) : ?>
 				<div class="media-object">
 					<div class="media-object-section">
-				 		<h3><a href="<?php echo $post->link;?>"><?php echo $post->title->rendered;?></a></h3>
-				 		<p><?php echo $post->acf->ecpt_tagline;?></p>
+				 		<h3><a href="<?php echo $feature->link;?>"><?php echo $feature->title->rendered;?></a></h3>
+				 		<p><?php echo $feature->acf->ecpt_tagline;?></p>
 					</div>
 				</div> 
 			<?php endforeach;?>
