5c649d975f12dba147a9eff67b31e98807cac1d0
ksascomm <ksascomm@gmail.com>
2020-03-16 13:36:51 -0400

    covid updates

--- page-covid19.php
+++ page-covid19.php
@@ -15,7 +15,7 @@
 <div class="main-wrap full-width" role="main">
  <?php do_action( 'foundationpress_before_content' ); ?>
 	<div class="row">
-		<div class="small-12 large-8 columns">
+		<div class="small-12 large-9 columns">
 		<?php foundationpress_breadcrumb();?>
 		 <?php while ( have_posts() ) : the_post(); ?>	
 		   <article <?php post_class('main-content covid') ?> id="post-<?php the_ID(); ?>">
@@ -23,6 +23,14 @@
 		       	<div class="callout warning">
 		   			<p>Page last modified: <strong><?php the_modified_time('F j, Y'); ?> at <?php the_modified_time('g:i a'); ?></strong></p>
 		   		</div>
+				<div class="callout secondary resources">
+					<h2>University Resources</h2>
+					<ul>
+						<li><strong><a href="https://hub.jhu.edu/novel-coronavirus-information/" target="_blank" rel="noopener noreferrer">University Novel coronavirus hub</a></strong> has resources and frequent updates for entire the Johns Hopkins University community</li>
+						<li><strong><a href="https://coronavirus.jhu.edu/" target="_blank" rel="noopener noreferrer">Johns Hopkins Coronavirus Resource Center</a></strong> has resources to help advance the understanding of the virus, inform the public, and brief policymakers in order to guide a response, improve care, and save lives.</li>
+						<li><strong><a href="https://ois.jhu.edu/Immigration_and_Visas/Travel_Information/COVID-19_Immigration-Related_FAQs/index.html">JHU OIS Immigration and Visa Travel Information</a></strong> has immigration-related FAQs.</li>
+					</ul>
+				  </div>		   		
 		       <?php do_action( 'foundationpress_page_before_entry_content' ); ?>
 		       <div class="entry-content">
 
@@ -34,7 +42,6 @@
 
 		<?php do_action( 'foundationpress_after_content' ); ?>
 		</div>
-		<?php get_sidebar(); ?>
  	</div>
  </div>
  <?php get_footer();
