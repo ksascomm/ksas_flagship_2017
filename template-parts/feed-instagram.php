e892eb630c4483fa167e51fd076b62752af49533
ksascomm <ksascomm@gmail.com>
2020-03-31 15:09:55 -0400

    Remove unused API/search functions

--- template-parts/feed-instagram.php
+++
@@ -1,4 +0,0 @@
-<div class="instagram-images">
-    <?php echo do_shortcode('[instagram-feed disablelightbox=true]');?>
-</div>
-<a class="button hollow float-right gram-link" href="https://www.instagram.com/JHUArtsSciences/">View on Instagram</a>
