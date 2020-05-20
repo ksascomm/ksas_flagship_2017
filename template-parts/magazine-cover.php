d893721b6ee7f7d3e1a70f8154abdc5eb8f72f6f
ksascomm <ksascomm@gmail.com>
2020-05-20 12:08:10 -0400

    fix API call; change variables

--- template-parts/magazine-cover.php
+++ template-parts/magazine-cover.php
@@ -1,32 +1,32 @@
 <?php
-	$frontpagefeaturedimg_url = 'https://magazine.krieger.jhu.edu/wp-json/wp/v2/media/13048';
+	$frontpagecoverimg_url = 'https://magazine.krieger.jhu.edu/wp-json/wp/v2/pages?_embed&slug=home';
 	
-	if ( false === ( $frontpageimage = get_transient( 'asmagazine_image_query' ) ) ) {
-		$frontpageimage = wp_remote_get($frontpagefeaturedimg_url);
-		set_transient( 'asmagazine_image_query', $frontpageimage, 2419200 ); 
+	if ( false === ( $frontpagecover = get_transient( 'asmagazine_cover_image_query' ) ) ) {
+		$frontpagecover = wp_remote_get($frontpagecoverimg_url);
+		set_transient( 'asmagazine_cover_image_query', $frontpagecover, 2419200 ); 
 	}		
 	
 	// Display a error nothing is returned.
-	if ( is_wp_error( $frontpageimage ) ) {
-		$error_string = $frontpageimage->get_error_message();
+	if ( is_wp_error( $frontpagecover ) ) {
+		$error_string = $frontpagecover->get_error_message();
 		echo '<div class="callout alert"><p>' . $error_string . '</p></div>';
 
 	}
 	// Get the body.
 	
-	$frontpost = json_decode( wp_remote_retrieve_body( $frontpageimage ) );
+	$frontcover = json_decode( wp_remote_retrieve_body( $frontpagecover ) );
 
 	// Display a warning nothing is returned.
-	if ( empty( $frontpost ) ) {
+	if ( empty( $frontcover ) ) {
 		echo '<div class="callout warning"><p>There is no home page featured image</p></div>';
 	}
 	
 	// If there is a featured image for the home page, display it!
-	if ( ! empty( $frontpost ) ) :?>
-		<?php foreach ( $frontpost as $front ) : ?>
+	if ( ! empty( $frontcover ) ) :?>
+		<?php foreach ( $frontcover as $cover ) : ?>
 			<div class="issue-cover">
 				<div class="card">
-					<img class="cover" src="<?php echo $front->_embedded->{'wp:featuredmedia'}[0]->media_details->sizes->{'medium_large'}->source_url;?>" alt="<?php echo $front->_embedded->{'wp:featuredmedia'}[0]->alt_text;?>">
+					<img class="cover" src="<?php echo $cover->_embedded->{'wp:featuredmedia'}[0]->media_details->sizes->{'medium_large'}->source_url;?>" alt="<?php echo $cover->_embedded->{'wp:featuredmedia'}[0]->alt_text;?>">
 				</div>
 			</div>
 		<?php endforeach;?>
