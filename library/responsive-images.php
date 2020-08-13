278931a9bd31fd728642f3ecfd8daf6c8cca6746
ksascomm <ksascomm@gmail.com>
2020-08-13 09:05:51 -0400

    fix Remove Width and Height Attributes function

--- library/responsive-images.php
+++ library/responsive-images.php
@@ -60,9 +60,11 @@ function foundationpress_adjust_image_sizes_attr( $sizes, $size ) {
 }
 add_filter( 'wp_calculate_image_sizes', 'foundationpress_adjust_image_sizes_attr', 10 , 2 );
 
-// Remove inline width and height attributes for post thumbnails
-function remove_thumbnail_dimensions( $html, $post_id, $post_image_id ) {
-	$html = preg_replace( '/(width|height)=\"\d*\"\s/', '', $html );
-	return $html;
+// Remove inline width and height attributes for post thumbnails & image insert
+add_filter( 'post_thumbnail_html', 'remove_thumbnail_dimensions', 10 );
+add_filter( 'image_send_to_editor', 'remove_thumbnail_dimensions', 10 );
+
+function remove_thumbnail_dimensions( $html ) {
+ 	$html = preg_replace( '/(width|height)=\"\d*\"\s/', "", $html );
+    return $html;
 }
-add_filter( 'post_thumbnail_html', 'remove_thumbnail_dimensions', 10, 3 );
